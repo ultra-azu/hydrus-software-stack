3fe99b1b64b9a10664338f12f8005f70363c6657
fernandopizarro1 <fernando.pizarro1@upr.edu>
2025-02-19 21:27:31 -0800

    rviz prompted now

--- camera-entrypoint.sh
+++ camera-entrypoint.sh
@@ -2,4 +2,16 @@
 set -e
 . devel/setup.sh && \
 	catkin_make -DCMAKE_BUILD_TYPE=Release -DCUDA_CUDART_LIBRARY=/usr/local/cuda/lib64/libcudart.so
-roslaunch zed_display_rviz display_zed2i.launch   
+
+trap "exit" SIGINT
+
+while true; do
+	echo "Do you want to display the ZED2i camera in RViz?"
+	read -p "(y/n)" choice
+
+	case $choice in 
+		[Yy]* ) roslaunch zed_display_rviz display_zed2i.launch; break;;
+		[Nn]* ) roslaunch --wait zed_wrapper zed2i.launch; break;;
+		* ) echo "Invalid input. Please try again.";;
+	esac
+done
